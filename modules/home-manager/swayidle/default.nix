31a5e8b268f6a8c80c0472e4ee50fc9713e32c54
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-08-24 22:34:28 +0200

    I hate swayidle

--- modules/home-manager/swayidle/default.nix
+++ modules/home-manager/swayidle/default.nix
@@ -3,6 +3,7 @@ let
 	vars = import ../../../hosts/desktop/vars.nix;
 	systemdTarget = if vars.display.vm == "Hyprland" then "hyprland-session.target" else "graphical-session.target";
 	swaylock = "${pkgs.swaylock}/bin/swaylock";
+	lockScript = "~/.local/bin/swaylock";
 	timeout = 10 * 60;
 in
 {
@@ -12,8 +13,17 @@ in
 		timeouts = [
 			{
 				timeout = timeout;
-				command = "${swaylock} -f";
+				command = "${lockScript}";
 			}
 		];
+	};
+
+	home.file.".local/bin/swaylock" = { #TODO: THIS DOES NOT WORK
+		text = ''
+			if ! hyprctl clients | grep 'fullscreen: 1'; then
+				${swaylock} -f
+			fi
+		'';
+		executable = true;
 	};
 }
