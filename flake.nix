19bb7f7f00ccabb41d3b8b0638972fa0a9102561
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-09-18 14:42:26 +0200

    formatted 2 files with nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -1,51 +1,51 @@
 {
-	description = "Nix configurations, Raphaël Weis";
+  description = "Nix configurations, Raphaël Weis";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		home-manager = {
-			url = "github:nix-community/home-manager";	
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		hyprland.url = "github:hyprwm/Hyprland";
-		xremap.url = "github:xremap/nix-flake";
-    	nixvim.url = "github:nix-community/nixvim";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+    xremap.url = "github:xremap/nix-flake";
+    nixvim.url = "github:nix-community/nixvim";
+  };
 
-	outputs = inputs @ { self, nixpkgs, home-manager, hyprland, xremap, nixvim, ... }:
-		let
-			inherit (self) outputs;
-			lib = nixpkgs.lib // home-manager.lib;
-			system = "x86_64-linux";
-			pkgs = import nixpkgs {
-				inherit system;
-				config.allowUnfree = true;
-			};
-			desktopVars = import ./hosts/desktop/vars.nix;
-		in
-		{
-			nixosConfigurations = {
-				desktop = let host = "desktop"; in lib.nixosSystem {
-					modules = [
-						./hosts/desktop/configuration.nix
-						xremap.nixosModules.default
-					];
-					specialArgs = {
-						inherit pkgs hyprland host;
-					};
-				};
-			};
-			homeConfigurations = {
-				"raphaelw@desktop" = let host = "home"; in lib.homeManagerConfiguration {
-					inherit pkgs;
-					modules = [ 
-						./hosts/home/home.nix
-        				hyprland.homeManagerModules.default
-					];
-					extraSpecialArgs = {
-						inherit pkgs host hyprland;
-					};
-				};
-			};
-		};
+  outputs = inputs @ { self, nixpkgs, home-manager, hyprland, xremap, nixvim, ... }:
+    let
+      inherit (self) outputs;
+      lib = nixpkgs.lib // home-manager.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      desktopVars = import ./hosts/desktop/vars.nix;
+    in
+    {
+      nixosConfigurations = {
+        desktop = let host = "desktop"; in lib.nixosSystem {
+          modules = [
+            ./hosts/desktop/configuration.nix
+            xremap.nixosModules.default
+          ];
+          specialArgs = {
+            inherit pkgs hyprland host;
+          };
+        };
+      };
+      homeConfigurations = {
+        "raphaelw@desktop" = let host = "home"; in lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./hosts/home/home.nix
+            hyprland.homeManagerModules.default
+          ];
+          extraSpecialArgs = {
+            inherit pkgs host hyprland;
+          };
+        };
+      };
+    };
 }
--- hosts/home/home.nix
+++ hosts/home/home.nix
@@ -1,54 +1,54 @@
 { pkgs, ... }:
 let
-	vars = import ./vars.nix;
+  vars = import ./vars.nix;
 in
 {
-	imports = [
-		(import ../../assets) 
-		(import ../../modules/home-manager/alacritty) 
-		(import ../../modules/home-manager/fzf) 
-		(import ../../modules/home-manager/gh) 
-		(import ../../modules/home-manager/git) 
-		(import ../../modules/home-manager/gnome-keyring) 
-		(import ../../modules/home-manager/gtk) 
-		(import ../../modules/home-manager/hyprpaper) 
-		(import ../../modules/home-manager/starship) 
-		(import ../../modules/home-manager/swayidle)
-		(import ../../modules/home-manager/tmux) 
-		(import ../../modules/home-manager/zsh) 
-		(import ../../modules/home-manager/waybar)
-		(import ../../modules/home-manager/swaylock)
-		(import ../../modules/home-manager/hyprland)
-	];
+  imports = [
+    (import ../../assets)
+    (import ../../modules/home-manager/alacritty)
+    (import ../../modules/home-manager/fzf)
+    (import ../../modules/home-manager/gh)
+    (import ../../modules/home-manager/git)
+    (import ../../modules/home-manager/gnome-keyring)
+    (import ../../modules/home-manager/gtk)
+    (import ../../modules/home-manager/hyprpaper)
+    (import ../../modules/home-manager/starship)
+    (import ../../modules/home-manager/swayidle)
+    (import ../../modules/home-manager/tmux)
+    (import ../../modules/home-manager/zsh)
+    (import ../../modules/home-manager/waybar)
+    (import ../../modules/home-manager/swaylock)
+    (import ../../modules/home-manager/hyprland)
+  ];
 
-	home = {
-		username = "raphaelw";
-		homeDirectory = "/home/raphaelw";
-		stateVersion = "22.11";
-		packages = with pkgs; [
-			bluetuith
-			alacritty
-			tmux
-			starship
-			neovim
-			lsd
-			cliphist
-			hyprpaper
-			pavucontrol
-			vscode
-			networkmanagerapplet
-			acpi
-			gnome.nautilus
-			gnome.gnome-themes-extra
-			gtk-engine-murrine
-			gruvbox-dark-icons-gtk
-			discord
-			brave
-			firefox
-		];
-	};
+  home = {
+    username = "raphaelw";
+    homeDirectory = "/home/raphaelw";
+    stateVersion = "22.11";
+    packages = with pkgs; [
+      bluetuith
+      alacritty
+      tmux
+      starship
+      neovim
+      lsd
+      cliphist
+      hyprpaper
+      pavucontrol
+      vscode
+      networkmanagerapplet
+      acpi
+      gnome.nautilus
+      gnome.gnome-themes-extra
+      gtk-engine-murrine
+      gruvbox-dark-icons-gtk
+      discord
+      brave
+      firefox
+    ];
+  };
 
-	programs = {
-		home-manager.enable = true;
-	};
+  programs = {
+    home-manager.enable = true;
+  };
 }
