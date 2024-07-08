1bf60555b02e124100a48f47c1ed89c7163f4a28
Raphaël Weis <raphael.weis@outlook.com>
2024-07-08 09:39:17 +0200

    feat(alacritty): Format config, added window padding settings.

--- homeManagerModules/features/alacritty.nix
+++ homeManagerModules/features/alacritty.nix
@@ -1,19 +1,19 @@
 { lib, config, ... }: {
   options.rFeatures.alacritty = {
-      enable = lib.mkEnableOption "the alacritty terminal emulator";
-      fontSize = lib.mkOption {
-        type = lib.types.int;
-        default = 12;
-        description = "The font size to use inside the terminal.";
-      };
-      winitX11ScaleFactor = lib.mkOption {
-        type = lib.types.int;
-        default = 1;
-        description = ''
-          The scale factor to set for alacrity. Usually 1, but in the case of a
-          high resolution screen, you can set it to 2.
-        '';
-      };
+    enable = lib.mkEnableOption "the alacritty terminal emulator";
+    fontSize = lib.mkOption {
+      type = lib.types.int;
+      default = 12;
+      description = "The font size to use inside the terminal.";
+    };
+    winitX11ScaleFactor = lib.mkOption {
+      type = lib.types.int;
+      default = 1;
+      description = ''
+        The scale factor to set for alacrity. Usually 1, but in the case of a
+        high resolution screen, you can set it to 2.
+      '';
+    };
   };
 
   config = lib.mkIf config.rFeatures.alacritty.enable {
@@ -25,7 +25,13 @@
           normal.family = "JetBrainsMonoNL NF";
         };
         mouse.hide_when_typing = true;
-        window.opacity = lib.mkForce 1;
+        window = {
+          opacity = lib.mkForce 1;
+          padding = {
+            x = 0;
+            y = 0;
+          };
+        };
         env."WINIT_X11_SCALE_FACTOR" =
           toString config.rFeatures.alacritty.winitX11ScaleFactor;
       };
