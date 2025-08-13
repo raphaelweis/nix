8de7781a95a34b311f55ab859f45a5a05820e25b
Raphaël Weis <raphael.weis.g@gmail.com>
2025-08-13 20:00:57 +0200

    feat: Added vague theme for ghostty.

--- homeManagerModules/ghostty.nix
+++ homeManagerModules/ghostty.nix
@@ -7,8 +7,7 @@
     programs.ghostty = {
       enable = true;
       settings = {
-        theme = "Adwaita Dark";
-        background = "#141415";
+        theme = "vague";
         font-family = "monospace";
         font-size = 14;
         shell-integration-features = "no-cursor";
@@ -20,6 +19,39 @@
         keybind = [
           "ctrl+enter=unbind"
         ];
+      };
+      themes = {
+        # Vague for Ghostty
+        # -------------------
+        # Theme Author: vague2k <ilovedrawing056@gmail.com>
+        # Ported by:	skewb1k <skewb1kunix@gmail.com>
+        # License:  	MIT License
+        # Upstream: 	https://github.com/vague2k/vague.nvim/blob/main/extras/ghostty/vague
+        vague = {
+          palette = [
+            "0=#252530"
+            "1=#d8647e"
+            "2=#7fa563"
+            "3=#f3be7c"
+            "4=#6e94b2"
+            "5=#bb9dbd"
+            "6=#aeaed1"
+            "7=#cdcdcd"
+            "8=#606079"
+            "9=#e08398"
+            "10=#99b782"
+            "11=#f5cb96"
+            "12=#8ba9c1"
+            "13=#c9b1ca"
+            "14=#bebeda"
+            "15=#d7d7d7"
+          ];
+          background = "#141415";
+          foreground = "#cdcdcd";
+          cursor-color = "#cdcdcd";
+          selection-background = "#252530";
+          selection-foreground = "#cdcdcd";
+        };
       };
     };
   };
