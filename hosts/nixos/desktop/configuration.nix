2ab8a2e281be50192f34a0232c751c864c62ea6e
Raphaël Weis <raphael.weis.g@gmail.com>
2025-08-22 13:46:22 +0200

    feat(hosts): Added a fix for suspend not working on the desktop.

--- hosts/nixos/desktop/configuration.nix
+++ hosts/nixos/desktop/configuration.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ ... }:
 {
   imports = [ ./hardware-configuration.nix ];
 
@@ -16,6 +16,30 @@
 
   hardware = {
     cpu.amd.updateMicrocode = true;
+  };
+
+  # On my motherboard (Gigabyte Technology Co., Ltd. X570S AORUS ELITE AX),
+  # A sensor wrongly triggers after suspend, immediately causing the computer to
+  # shut down. This service runs:
+  # echo disabled > /sys/class/thermal/thermal_zone1/mode
+  # at boot to disable this sensor and prevent this problem, since it's likely a
+  # hardware issue on the motherboard.
+  # Note that this may cause a failure to detect an actual overheating problem,
+  # since we are disabling the sensor.
+  # https://forum.manjaro.org/t/acpi-thermal-limit-triggers-thermal-shutdown-on-sleep/154502/16
+  # Note: I tried using systemd.tmpfiles.rules instead but it didn't work because
+  # something was overriding the value after the rule was being set, so instead I
+  # use a systemd service file with the line: after = [ "multi-user.target" ] to
+  # make sure it runs late enough.
+  systemd.services.gigabyte-sensor-fix = {
+    description = "Gigabyte X570 Platform Sensor Fix";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = ''/bin/sh -c "echo disabled > /sys/class/thermal/thermal_zone1/mode"'';
+    };
   };
 
   system.stateVersion = "25.05";
