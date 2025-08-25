16e19ec502bbad433b7d807fe709643c4cf8ff30
Raphaël Weis <raphael.weis.g@gmail.com>
2025-08-25 23:33:59 +0200

    feat(tmux): Added the tmux sessionizer script with a zsh keybind to trigger it.

--- homeManagerModules/zsh.nix
+++ homeManagerModules/zsh.nix
@@ -12,13 +12,16 @@
       enableCompletion = true;
       defaultKeymap = "emacs";
       initContent =
-        lib.mkOrder 500 # bash
+        lib.mkOrder 1500 # bash
           ''
             autoload -U colors && colors
             PS1="%{$fg[yellow]%}%~%{$fg[red]%} %{$reset_color%}$%b "
 
             alias ta="tmux attach"
             alias la="ls -la"
+
+            # Assign Alt+S to run the tmux-sessionizer script defined in [tmux.nix]
+            bindkey -s '^[s' 'tmux-sessionizer\n'
           '';
     };
   };
